bba2a16d05b9ed5bd26373177cca386a359576bc
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-16 13:36:38 -0400

    added the publishable methods

--- publishable/lib/publishable.rb
+++ publishable/lib/publishable.rb
@@ -1,6 +1,15 @@
 require "publishable/version"
 
 module Publishable
-  class Error < StandardError; end
-  # Your code goes here...
+  def publish 
+    update(published_at: Time.now)
+  end
+
+  def published?
+    published_at.present?
+  end
+
+  def unpublish
+    update(published_at: nil)
+  end
 end
